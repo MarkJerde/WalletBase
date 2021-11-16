98bf90d918501bfa64b057b6b22a0f82cc45b5d7
Mark Jerde <mark.a.jerde@gmail.com>
2021-11-15 22:36:49 -0600

    Add Xcode project template

---
+++ WalletBase/WalletBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletBaseApp.swift
+//  WalletBase
+//
+//  Created by Mark Jerde on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WalletBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
