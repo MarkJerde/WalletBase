7812fd527a9edb44569057d75c745e7973acf848
Mark Jerde <mark.a.jerde@gmail.com>
2022-08-03 22:33:44 -0500

    Improve file organization

--- WalletBase/Database/SQLite/SQLiteDatabase.swift
+++ WalletBase/Database/SQLite/SQLiteDatabase.swift
@@ -8,26 +8,6 @@
 import Foundation
 import SQLite3
 
-/// A type that can be decoded from an SQLite statement beginning at a given column.
-protocol SQLiteDatabaseItem {
-	/// Creates a new instance by decoding from the given statement.
-	/// - Parameters:
-	///   - statement: The statement to decode from.
-	///   - column: The column at which to start decoding. Multiple columns may be consumed if it is a composite type.
-	///   - nextColumn: A closure to which is passed the next column number, to provide incrementing.
-	/// - Returns: The decoded instance.
-	static func decode(from statement: OpaquePointer, column: Int32, nextColumn: ((Int32) -> Void)?) -> Self?
-}
-
-protocol SQLiteTable {
-	var name: String { get }
-}
-
-protocol SQLiteQueryDescribing {
-	static var columns: [String] { get }
-	static var table: SQLiteTable { get }
-}
-
 /// A wrapper around an SQLite database session.
 class SQLiteDatabase {
 	/// The database file.
---
+++ WalletBase/Database/SQLite/SQLiteProtocols.swift
@@ -0,0 +1,28 @@
+//
+//  SQLiteProtocols.swift
+//  WalletBase
+//
+//  Created by Mark Jerde on 8/2/22.
+//
+
+import Foundation
+
+/// A type that can be decoded from an SQLite statement beginning at a given column.
+protocol SQLiteDatabaseItem {
+	/// Creates a new instance by decoding from the given statement.
+	/// - Parameters:
+	///   - statement: The statement to decode from.
+	///   - column: The column at which to start decoding. Multiple columns may be consumed if it is a composite type.
+	///   - nextColumn: A closure to which is passed the next column number, to provide incrementing.
+	/// - Returns: The decoded instance.
+	static func decode(from statement: OpaquePointer, column: Int32, nextColumn: ((Int32) -> Void)?) -> Self?
+}
+
+protocol SQLiteTable {
+	var name: String { get }
+}
+
+protocol SQLiteQueryDescribing {
+	static var columns: [String] { get }
+	static var table: SQLiteTable { get }
+}
