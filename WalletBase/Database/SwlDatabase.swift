3866422d855f0d1cb4f2b389504e8be89c374f28
Mark Jerde <mark.a.jerde@gmail.com>
2022-08-03 21:52:58 -0500

    Add ability to edit data

--- WalletBase/Database/SwlDatabase.swift
+++ WalletBase/Database/SwlDatabase.swift
@@ -27,9 +27,7 @@ class SwlDatabase {
 	///   - completion: A closure to call after the decryption key is established or will not be established.
 	func open(password: (@escaping (String) -> Void) -> Void, completion: @escaping (Bool) -> Void) {
 		// Ensure the user is warned as to the safety of this software. This is a two-part mechanism. First we hash the file to create a UserDefaults key and if it failed to hash or the key was not true in UserDefaults, show the alert. If the user does not accept the alert, opening the wallet fails and the completion is called accordingly. Second, after the user has accepted the alert and provided a password capable of unlocking the file, then save true to that UserDefaults key. This provides a disclaimer before the user has provided any sensitive data (the encrypted file is not sensitive because they already have that readable by their account on the device) and does not persist acceptance until unlocking verifies that a person with access to the file contents provided that acceptance. This will continue showing the disclaimer for each file until all are accepted so that if multiple users shared a device they would each be independently warned.
-		let sha256 = database.file.contentSHA256
-		let disclaimerKey = "acceptedTerms_\(sha256?.compactMap { String(format: "%02x", $0) }.joined() ?? "noHash")"
-		if sha256 == nil || !UserDefaults.standard.bool(forKey: disclaimerKey) {
+		if !hasAcceptedTerms(for: database.file) {
 			let alert = NSAlert()
 			alert.messageText = "Use at your own risk!"
 			alert.informativeText = "THE AUTHOR SUPPLIES THIS SOFTWARE \"AS IS\", AND MAKES NO EXPRESS OR IMPLIED WARRANTY OF ANY KIND. This software was written without expertise in data security and may have vulnerabilities which enable other software to capture your decrypted information, defects which result in a loss of data, or other ill effects. Users are encouraged to read the source code, made publicly available."
@@ -68,14 +66,37 @@ class SwlDatabase {
 			}
 
 			// Store that the user has seen the warning, now that we know the user is someone who can unlock the file.
-			UserDefaults.standard.set(true, forKey: disclaimerKey)
+			self.acceptTerms(for: self.database.file)
 
+			self.isUnlocked = true
 			completion(true)
 		})
 	}
 
+	private func disclaimerKey(for file: URL) -> String? {
+		guard let sha256 = database.file.contentSHA256 else { return nil }
+		return "acceptedTerms_\(sha256.compactMap { String(format: "%02x", $0) }.joined())"
+	}
+
+	private func hasAcceptedTerms(for file: URL) -> Bool {
+		guard let disclaimerKey = disclaimerKey(for: file) else { return false }
+		return UserDefaults.standard.bool(forKey: disclaimerKey)
+	}
+
+	private func acceptTerms(for file: URL) {
+		guard let disclaimerKey = disclaimerKey(for: file) else { return }
+		UserDefaults.standard.set(true, forKey: disclaimerKey)
+	}
+
 	/// Closes the wallet by discarding the decryption key.
 	func close() {
+		// Update the terms acceptance if the file was edited.
+		if isUnlocked,
+		   !hasAcceptedTerms(for: database.file)
+		{
+			acceptTerms(for: database.file)
+		}
+		isUnlocked = false
 		crypto = nil
 	}
 
@@ -235,6 +256,65 @@ class SwlDatabase {
 		}
 	}
 
+	enum Error: Swift.Error {
+		case writeFailure
+		case writeFailureIndeterminite
+	}
+
+	func update(fieldValues: [SwlID: String], in cardId: SwlID) throws {
+		do {
+			try database.beginTransaction()
+		}
+		catch {
+			throw Error.writeFailure
+		}
+
+		for (id, value) in fieldValues {
+			let current: [CardFieldValue]? = try? database.select(where: "id \(id.queryCondition)").compactMap { $0 }
+			guard let current = current,
+			      current.count == 1,
+			      let current = current.first,
+			      current.cardId == cardId,
+			      let encryptedValue = crypto?.encrypt(value)
+			else {
+				do {
+					try database.rollbackTransaction()
+				}
+				catch {
+					throw Error.writeFailureIndeterminite // This should be indeterminite, since it did not fail in SQLite.
+				}
+				throw Error.writeFailure
+			}
+
+			let updated = CardFieldValue(id: current.id,
+			                             cardId: current.cardId,
+			                             templateFieldId: current.templateFieldId,
+			                             value: [UInt8](encryptedValue))
+			do {
+				try database.update(record: updated, from: current)
+			}
+			catch {
+				do {
+					try database.rollbackTransaction()
+				}
+				catch {
+					if error is SQLiteDatabase.DatabaseError {
+						throw Error.writeFailure // We can't be sure it is indeterminite, since SQLite returns an error in some rollback cases which occur after an automatically rollbacked error.
+					}
+					throw Error.writeFailureIndeterminite // This should be indeterminite, since it did not fail in SQLite.
+				}
+				throw Error.writeFailure
+			}
+		}
+
+		do {
+			try database.commitTransaction()
+		}
+		catch {
+			throw Error.writeFailureIndeterminite
+		}
+	}
+
 	/// Obtains the encrypted template field of a given id.
 	/// - Parameter category: The category.
 	/// - Returns: The items.
@@ -266,6 +346,7 @@ class SwlDatabase {
 
 	/// The cryptography provider which can decrypt this wallet.
 	private var crypto: CryptoProvider?
+	private var isUnlocked = false
 
 	enum Tables: String, SQLiteTable {
 		case databaseVersion = "spb_DatabaseVersion" // TODO:
