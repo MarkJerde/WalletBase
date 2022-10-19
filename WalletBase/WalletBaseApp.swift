c4a4ffa550c4232fef4464ba467ced77a30c62c4
Mark Jerde <mark.a.jerde@gmail.com>
2022-10-19 07:23:16 -0500

    Lock wallet on window or app close

--- WalletBase/WalletBaseApp.swift
+++ WalletBase/WalletBaseApp.swift
@@ -7,13 +7,27 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationWillTerminate(_ notification: Notification) {
+		// Signal inactivity to lock the wallet.
+		ActivityMonitor.shared.onInactivity?()
+	}
+}
+
 @main
 struct WalletBaseApp: App {
+#if os(macOS)
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+#endif
+
 	var body: some Scene {
 		WindowGroup {
 #if os(macOS)
 			MainView()
 				.frame(minWidth: 976, maxWidth: .infinity, minHeight: 576, maxHeight: .infinity, alignment: .center)
+				.onDisappear {
+					ActivityMonitor.shared.onInactivity?()
+				}
 #else
 			MainView()
 #endif
