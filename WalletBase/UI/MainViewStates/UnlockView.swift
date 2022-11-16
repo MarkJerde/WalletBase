16f5a4573ffdd22790526d37318e94bc3c8fa17f
Mark Jerde <mark.a.jerde@gmail.com>
2022-11-15 21:15:56 -0600

    Fix button selection
    This previously only worked on appearance while in foreground
    but did not work on auto-lock when the app is in the background.

--- WalletBase/UI/MainViewStates/UnlockView.swift
+++ WalletBase/UI/MainViewStates/UnlockView.swift
@@ -39,8 +39,28 @@ struct UnlockView: View {
 	}
 
 	func makeUnlockButtonFirstResponder() {
+		let buttonOfWindow: (NSWindow) -> NSButton? = { window in
+			guard let firstSubviews = window.contentViewController?.view.subviews,
+				  firstSubviews.count > 1,
+				  let secondSubviews = firstSubviews.prefix(2).last?.subviews,
+				  !secondSubviews.isEmpty,
+				  let button = secondSubviews[0] as? NSButton else { return nil }
+			return button
+		}
+
+		let availableWindows: [NSWindow] = {
+			var response: [NSWindow] = []
+			let mainWindow = NSApplication.shared.mainWindow
+			if let mainWindow = mainWindow {
+				response.append(mainWindow)
+			}
+			let otherWindows = NSApplication.shared.windows.filter { $0 != mainWindow }
+			response.append(contentsOf: otherWindows)
+			return response
+		}()
+
 		makeViewFirstResponder {
-			guard let button = NSApplication.shared.mainWindow?.contentViewController?.view.subviews[1].subviews[0] as? NSButton else { return nil }
+			guard let button = availableWindows.compactMap({ buttonOfWindow($0) }).first else { return nil }
 			// Adapted from https://stackoverflow.com/a/31730015
 			let key = String(utf16CodeUnits: [unichar(NSCarriageReturnCharacter)], count: 1) as String
 			button.keyEquivalent = key
