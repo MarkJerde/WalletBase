e2211ef0544fc8fff3af6b3848cfd1fdca5243c2
Mark Jerde <mark.a.jerde@gmail.com>
2023-03-20 22:44:00 -0500

    Add extension to erase to AnyView
    Because sometimes you have to.

---
+++ WalletBase/Util/View+EraseToAnyView.swift
@@ -0,0 +1,14 @@
+//
+//  View+EraseToAnyView.swift
+//  WalletBase
+//
+//  Created by Mark Jerde on 3/20/23.
+//
+
+import SwiftUI
+
+extension View {
+	var eraseToAnyView: AnyView {
+		AnyView(self)
+	}
+}
