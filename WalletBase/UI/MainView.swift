7e4724cdf71687e6bd1d2bf9216c60784050b565
Mark Jerde <mark.a.jerde@gmail.com>
2023-05-25 22:55:36 -0500

    Add new card/folder menu

--- WalletBase/UI/MainView.swift
+++ WalletBase/UI/MainView.swift
@@ -41,6 +41,125 @@ struct MainView: View {
 		}
 	}
 
+	struct Prompt {
+		let title: String?
+		let message: String?
+		let options: [MenuView.Option]
+		let handler: (String, [String: String]) -> Void
+	}
+
+	@State private var prompt: Prompt? = nil
+
+	private func newFolderOrCardPrompt(in database: SwlDatabase, category: SwlDatabase.Category?) -> Prompt {
+		let options: [CreateNew]
+		if category == nil {
+			options = [
+				.folder,
+				.cancel,
+			]
+		} else {
+			options = CreateNew.allCases
+		}
+		return Self.promptToCreateNew(options: options) { response in
+			let prompt: String
+			let fieldName: String
+			let isNewFolder = response == CreateNew.folder.rawValue.capitalized
+			if isNewFolder {
+				prompt = "Create new folder"
+				fieldName = "Folder name"
+			} else {
+				prompt = "Create new card"
+				fieldName = "Card name"
+			}
+			self.prompt = Self.promptForField(
+				prompt: prompt,
+				fieldName: fieldName,
+				completion: { newName in
+					if isNewFolder {
+						self.createFolder(named: newName, in: database, category: category)
+					} else if let category {
+						self.createCard(named: newName, in: database, category: category)
+					}
+					self.prompt = nil
+					// Navigate to reload the content.
+					self.navigate(toDatabase: database, category: self.category, card: nil)
+				}, cancel: {
+					self.prompt = nil
+				}, error: { errorDetail in
+					guard errorDetail.hasPrefix("missing: "),
+					      let missingItem = errorDetail.components(separatedBy: "missing: ").last,
+					      !missingItem.isEmpty
+					else {
+						self.prompt = .init(title: "Error",
+						                    message: "An error occurred.",
+						                    options: [
+						                    	.button(text: "Okay"),
+						                    ], handler: { _, _ in
+						                    	self.prompt = nil
+						                    })
+						return
+					}
+
+					self.prompt = .init(title: "Error",
+					                    message: "\(missingItem) is required.",
+					                    options: [
+					                    	.button(text: "Okay"),
+					                    ], handler: { _, _ in
+					                    	self.prompt = nil
+					                    })
+				})
+		} cancel: {
+			prompt = nil
+		}
+	}
+
+	private static func promptToCreateNew(options: [CreateNew], completion: @escaping (String) -> Void, cancel: @escaping () -> Void) -> Prompt {
+		.init(
+			title: "Create new:",
+			message: nil,
+			options: options
+				.map {
+					$0.rawValue.capitalized
+				}
+				.map {
+					.button(text: $0)
+				},
+			handler: { selection, _ in
+				guard selection != CreateNew.cancel.rawValue.capitalized else {
+					cancel()
+					return
+				}
+
+				completion(selection)
+			})
+	}
+
+	private static func promptForField(prompt: String, fieldName: String, completion: @escaping (String) -> Void, cancel: @escaping () -> Void, error: @escaping (String) -> Void) -> Prompt {
+		.init(
+			title: prompt,
+			message: nil,
+			options: [
+				.field(id: fieldName),
+				.button(text: "Create"),
+				.button(text: "Cancel"),
+			],
+			handler: { selection, fieldValues in
+				guard selection != "Cancel" else {
+					cancel()
+					return
+				}
+
+				guard let value = fieldValues[fieldName],
+				      !value.isEmpty
+				else {
+					error("missing: \(fieldName)")
+					return
+				}
+
+				completion(value)
+			})
+	}
+
 	@State private var folder: WalletFile?
 	@State private var files: [WalletFile] = []
 	@State private var category: SwlDatabase.Item?
@@ -265,47 +384,55 @@ struct MainView: View {
 		state = .buttonToUnlock(databaseFile: database.file)
 	}
 
+	private enum CreateNew: String, CaseIterable {
+		case card
+		case folder
+		case cancel
+	}
+
 	var body: some View {
-		switch state {
-		case .loadingDatabase:
-			SandboxFileBrowser(folder: $folder,
-			                   files: $files) { item in
-				self.state = .buttonToUnlock(databaseFile: item.url)
-			} browse: {
-				self.loadFile()
-			}
-		case .buttonToUnlock(let databaseFile):
-			UnlockView(file: "\(databaseFile)", unlock: {
-				let database = SwlDatabase(file: databaseFile)
-				state = .unlocking(database: database)
-			}, importFile: FileStorage.contains(databaseFile) ? nil : {
-				guard let importedFile = FileStorage.importFile(at: databaseFile) else { return }
-				state = .buttonToUnlock(databaseFile: importedFile)
-			})
-		case .unlocking(let database):
-			Text("Unlocking...")
-				.onAppear {
-					database.open { completion in
-						state = .passwordPrompt(database: database, completion: completion)
-					} completion: { success in
-						guard success else {
-							state = .error(message: "Unable to open database",
+		ZStack {
+			Group {
+				switch state {
+				case .loadingDatabase:
+					SandboxFileBrowser(folder: $folder,
+					                   files: $files) { item in
+						self.state = .buttonToUnlock(databaseFile: item.url)
+					} browse: {
+						self.loadFile()
+					}
+				case .buttonToUnlock(let databaseFile):
+					UnlockView(file: "\(databaseFile)", unlock: {
+						let database = SwlDatabase(file: databaseFile)
+						state = .unlocking(database: database)
+					}, importFile: FileStorage.contains(databaseFile) ? nil : {
+						guard let importedFile = FileStorage.importFile(at: databaseFile) else { return }
+						state = .buttonToUnlock(databaseFile: importedFile)
+					})
+				case .unlocking(let database):
+					Text("Unlocking...")
+						.onAppear {
+							database.open { completion in
+								state = .passwordPrompt(database: database, completion: completion)
+							} completion: { success in
+								guard success else {
+									state = .error(message: "Unable to open database",
+									               then: .buttonToUnlock(databaseFile: database.file))
+									return
+								}
+
+								navigate(toDatabase: database)
+							}
+						}
+				case .passwordPrompt(let database, let completion):
+					PasswordPrompt { password in
+						guard let password = password else {
+							state = .error(message: "No password",
 							               then: .buttonToUnlock(databaseFile: database.file))
 							return
 						}
-
-						navigate(toDatabase: database)
+						completion(password)
 					}
-				}
-		case .passwordPrompt(let database, let completion):
-			PasswordPrompt { password in
-				guard let password = password else {
-					state = .error(message: "No password",
-					               then: .buttonToUnlock(databaseFile: database.file))
-					return
-				}
-				completion(password)
-			}
 #if DEBUG
 						.onAppear {
 							guard database.file.lastPathComponent == "Sample.swl",
@@ -316,126 +443,150 @@ struct MainView: View {
 							}
 						}
 #endif
-		case .browseContent(let database):
-			VStack {
-				ItemGrid(items: $items,
-				         container: $category,
-				         onItemTap: { item in
-				         	switch item.itemType {
-				         	case .card(let card):
-				         		navigate(toDatabase: database, category: category, card: card)
-				         	case .category:
-				         		navigate(toDatabase: database, category: item)
-				         	}
-				         },
-				         onBackTap: {
-				         	guard let category = category,
-				         	      case .category(let swlCategory) = category.itemType else { return }
-				         	let parentId = swlCategory.parent
-				         	let parent = database.categoryItem(forId: parentId)
-				         	// Clear the restore category so it won't try to keep restoring if we navigate back to the root.
-				         	restoreCategoryId = nil
-				         	navigate(toDatabase: database, category: parent)
-				         },
-				         onSearch: (self.category == nil ? { searchString in
-				         	guard !searchString.isEmpty else {
-				         		items = items(of: category, in: database)
-				         		return
-				         	}
-				         	items = items(of: searchString, in: database)
-				         	numCards = nil
-				         	cardIndex = nil
-				         	state = .browseContent(database: database)
-				         } : nil))
-				Button("Lock") {
-					lock(database: database)
-				}
-				.padding(.all, 20)
-			}
-		case .viewCard(let database):
-			VStack {
-				CardView(item: $card, onBackTap: {
-					navigate(toDatabase: database, category: category)
-				}, onPreviousTap: cardIndex == nil || cardIndex! <= 0 ? nil : {
-					guard let cardIndex = cardIndex else {
-						return
-					}
-
-					navigate(toDatabase: database, category: category, index: cardIndex - 1)
-				}, onNextTap: cardIndex == nil || numCards == nil || cardIndex! + 1 >= numCards! ? nil : {
-					guard let cardIndex = cardIndex else {
-						return
-					}
-
-					navigate(toDatabase: database, category: category, index: cardIndex + 1)
-				}, onSave: { edits, editedDescription in
-					guard let card = card else {
-						showFailedToSaveAlert()
-						return false
-					}
-					do {
-						try database.update(
-							fieldValues: Dictionary(uniqueKeysWithValues: edits.map { (key: CardValuesComposite<SwlDatabase.SwlID>.CardValue, value: String) in
-								let id = key.id
-								let idType: SwlDatabase.IDType
-								if id.value.isEmpty,
-								   let newId = SwlDatabase.SwlID.new
-								{
-									idType = .new(newId)
-								} else {
-									idType = .existing(id)
-								}
-								return (idType, (value, key.templateFieldId))
-							}),
-							editedDescription: editedDescription,
-							in: card.id)
-					} catch {
-						if let error = error as? SwlDatabase.Error {
-							switch error {
-							case .writeFailureIndeterminite:
-								let alert = NSAlert()
-								alert.messageText = "Save Failed"
-								alert.informativeText = "Something went wrong while trying to save. Enough so that the wallet may be corrupted. You should probably at least close the wallet, reopen it, and check to see if things look right."
-								alert.alertStyle = .critical
-								alert.addButton(withTitle: "Close Wallet")
-								alert.addButton(withTitle: "Take More Risks")
-								guard alert.runModal() == .alertFirstButtonReturn else { return false }
-								folder = nil
-								lock(database: database)
-								return true
-							default:
-								break
-							}
+				case .browseContent(let database):
+					VStack {
+						ItemGrid(items: $items,
+						         container: $category,
+						         onItemTap: { item in
+						         	switch item.itemType {
+						         	case .card(let card):
+						         		navigate(toDatabase: database, category: category, card: card)
+						         	case .category:
+						         		navigate(toDatabase: database, category: item)
+						         	}
+						         },
+						         onBackTap: {
+						         	guard let category = category,
+						         	      case .category(let swlCategory) = category.itemType else { return }
+						         	let parentId = swlCategory.parent
+						         	let parent = database.categoryItem(forId: parentId)
+						         	// Clear the restore category so it won't try to keep restoring if we navigate back to the root.
+						         	restoreCategoryId = nil
+						         	navigate(toDatabase: database, category: parent)
+						         },
+						         onNewTap: {
+						         	let createInCategory: SwlDatabase.Category?
+						         	if let category = category,
+						         	   case .category(let swlCategory) = category.itemType
+						         	{
+						         		createInCategory = swlCategory
+						         	} else {
+						         		createInCategory = nil
+						         	}
+						         	prompt = newFolderOrCardPrompt(in: database, category: createInCategory)
+						         },
+						         onSearch: (self.category == nil ? { searchString in
+						         	guard !searchString.isEmpty else {
+						         		items = items(of: category, in: database)
+						         		return
+						         	}
+						         	items = items(of: searchString, in: database)
+						         	numCards = nil
+						         	cardIndex = nil
+						         	state = .browseContent(database: database)
+						         } : nil))
+						Button("Lock") {
+							lock(database: database)
 						}
-						showFailedToSaveAlert()
-						return false
+						.padding(.all, 20)
 					}
-					// Navigate to updated card.
-					guard let cardIndex = cardIndex else {
-						navigate(toDatabase: database, category: category)
-						return true
+				case .viewCard(let database):
+					VStack {
+						CardView(item: $card, onBackTap: {
+							navigate(toDatabase: database, category: category)
+						}, onPreviousTap: cardIndex == nil || cardIndex! <= 0 ? nil : {
+							guard let cardIndex = cardIndex else {
+								return
+							}
+
+							navigate(toDatabase: database, category: category, index: cardIndex - 1)
+						}, onNextTap: cardIndex == nil || numCards == nil || cardIndex! + 1 >= numCards! ? nil : {
+							guard let cardIndex = cardIndex else {
+								return
+							}
+
+							navigate(toDatabase: database, category: category, index: cardIndex + 1)
+						}, onSave: { edits, editedDescription in
+							guard let card = card else {
+								showFailedToSaveAlert()
+								return false
+							}
+							do {
+								try database.update(
+									fieldValues: Dictionary(uniqueKeysWithValues: edits.map { (key: CardValuesComposite<SwlDatabase.SwlID>.CardValue, value: String) in
+										let id = key.id
+										let idType: SwlDatabase.IDType
+										if id.value.isEmpty,
+										   let newId = SwlDatabase.SwlID.new
+										{
+											idType = .new(newId)
+										} else {
+											idType = .existing(id)
+										}
+										return (idType, (value, key.templateFieldId))
+									}),
+									editedDescription: editedDescription,
+									in: card.id)
+							} catch {
+								if let error = error as? SwlDatabase.Error {
+									switch error {
+									case .writeFailureIndeterminite:
+										let alert = NSAlert()
+										alert.messageText = "Save Failed"
+										alert.informativeText = "Something went wrong while trying to save. Enough so that the wallet may be corrupted. You should probably at least close the wallet, reopen it, and check to see if things look right."
+										alert.alertStyle = .critical
+										alert.addButton(withTitle: "Close Wallet")
+										alert.addButton(withTitle: "Take More Risks")
+										guard alert.runModal() == .alertFirstButtonReturn else { return false }
+										folder = nil
+										lock(database: database)
+										return true
+									default:
+										break
+									}
+								}
+								showFailedToSaveAlert()
+								return false
+							}
+							// Navigate to updated card.
+							guard let cardIndex = cardIndex else {
+								navigate(toDatabase: database, category: category)
+								return true
+							}
+							navigate(toDatabase: database, category: category, index: cardIndex)
+							return true
+						})
+						Button("Lock") {
+							lock(database: database)
+						}
+						.padding(.all, 20)
 					}
-					navigate(toDatabase: database, category: category, index: cardIndex)
-					return true
-				})
-				Button("Lock") {
-					lock(database: database)
+				case .error(let message, let then):
+					VStack {
+						Text("Error:")
+						Text(message)
+						Button("Ok") {
+							state = then
+						}
+					}
+					.compatibilityKeyboardShortcut(.defaultAction) { window in
+						guard let firstSubviews = (window.contentViewController?.view ?? window.contentView)?.subviews,
+						      let secondSubviews = firstSubviews.prefix(3).last?.subviews,
+						      let button = secondSubviews.first as? NSButton else { return nil }
+						return button
+					}
 				}
-				.padding(.all, 20)
 			}
-		case .error(let message, let then):
-			VStack {
-				Text("Error:")
-				Text(message)
-				Button("Ok") {
-					state = then
-				}
-			}
-			.compatibilityKeyboardShortcut(.defaultAction) { window in
-				guard let firstSubviews = (window.contentViewController?.view ?? window.contentView)?.subviews,
-				      let secondSubviews = firstSubviews.prefix(3).last?.subviews,
-				      let button = secondSubviews.first as? NSButton else { return nil }
-				return button
+			.blur(radius: prompt == nil ? 0 : 5)
+
+			if let prompt {
+				MenuView(title: prompt.title,
+				         message: prompt.message,
+				         options: prompt.options,
+				         handler: prompt.handler)
+					.backgroundColor(.blue)
+					.foregroundColor(.white)
+					.fixedSize()
 			}
 		}
 	}
--- WalletBase/UI/MenuView.swift
+++ WalletBase/UI/MenuView.swift
@@ -11,19 +11,20 @@ struct MenuView: View {
 	internal init(title: String? = nil,
 	              message: String? = nil,
 	              options: [Option],
-	              handler: @escaping (String) -> Void)
+	              handler: @escaping (String, [String: String]) -> Void)
 	{
 		self.title = title
 		self.message = message
 		self.options = options
 		self.handler = handler
 		_backgroundColor = .init(initialValue: .tappableClear)
+		fieldValues = [String](repeating: "", count: options.count)
 	}
 
 	let title: String?
 	let message: String?
 	let options: [Option]
-	let handler: (String) -> Void
+	let handler: (String, [String: String]) -> Void
 
 	enum Option: Hashable {
 		case button(text: String)
@@ -31,6 +32,8 @@ struct MenuView: View {
 	}
 
 	@State private var backgroundColor: Color
+
+	@State private var fieldValues: [String]
 
 	var body: some View {
 		VStack(spacing: 0) {
@@ -55,14 +58,33 @@ struct MenuView: View {
 				switch item.element {
 				case .button(let text):
 					Button {
-						handler(text)
+						let fieldValuesDictionary: [String: String] = Dictionary(
+							uniqueKeysWithValues:
+							options
+								.enumerated()
+								.compactMap { item in
+									let key: String
+									switch item.element {
+									case .field(let title):
+										key = title
+									default:
+										return nil
+									}
+
+									let value = fieldValues[item.offset]
+
+									return (key, value)
+								}
+						)
+						handler(text, fieldValuesDictionary)
 					} label: {
 						Text(text)
 					}
 					.buttonStyle(OptionButtonStyle(backgroundColor: backgroundColor))
 					.eraseToAnyView
-				case .field:
-					Text("---")
+				case .field(let title):
+					TextField(title, text: $fieldValues[item.offset])
+						.foregroundColor(Color.black)
 						.eraseToAnyView
 				}
 				if item.offset < (options.count - 1) {
@@ -106,10 +128,10 @@ extension MenuView {
 struct MenuView_Previews: PreviewProvider {
 	static var previews: some View {
 		MenuView(title: "Title", message: "Message here", options: [
-			"Abort",
-			"Retry",
-			"Fail",
-		]) { selection in
+			.button(text: "Abort"),
+			.button(text: "Retry"),
+			.button(text: "Fail"),
+		]) { selection, _ in
 			NSLog("Selected \(selection)")
 		}
 		.backgroundColor(.yellow)
